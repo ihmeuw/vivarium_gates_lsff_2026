4f15336de63abbaf79e801a021ce2e19bf1cde95
Jim Albright <albrja@uw.edu>
2026-07-27 11:02:20 -0700

    Update files

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     with open(os.path.join(src_dir, "lsff_utils", "__about__.py")) as f:
         exec(f.read(), about)
 
-    with open(os.path.join(base_dir, "README.md")) as f:
+    with open(os.path.join(base_dir, "README.rst")) as f:
         long_description = f.read()
 
     setup(
