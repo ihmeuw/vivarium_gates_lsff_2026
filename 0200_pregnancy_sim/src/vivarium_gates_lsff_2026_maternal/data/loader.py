11a6a47462567fbd279cfe0067b705428b743fb1
Jim Albright <albrja@uw.edu>
2026-07-29 09:29:21 -0700

    Fix duplicate index level

--- 0200_pregnancy_sim/src/vivarium_gates_lsff_2026_maternal/data/loader.py
+++ 0200_pregnancy_sim/src/vivarium_gates_lsff_2026_maternal/data/loader.py
@@ -148,11 +148,13 @@ def load_population_location(key: str, location: str, mean_draw: bool) -> str:
 def load_population_structure(key: str, location: str, mean_draw: bool) -> pd.DataFrame:
     base_population_structure = interface.get_population_structure(location)
     pregnancy_end_rate_avg = get_pregnancy_end_incidence(location, mean_draw)
-    pregnant_population_structure = (
-        pregnancy_end_rate_avg.multiply(base_population_structure["value"], axis=0)
-        .assign(location=location)
-        .set_index("location", append=True)
+    pregnant_population_structure = pregnancy_end_rate_avg.multiply(
+        base_population_structure["value"], axis=0
     )
+    if "location" not in pregnant_population_structure.index.names:
+        pregnant_population_structure = pregnant_population_structure.assign(
+            location=location
+        ).set_index("location", append=True)
     return vi_utils.sort_hierarchical_data(pregnant_population_structure)
 
 
