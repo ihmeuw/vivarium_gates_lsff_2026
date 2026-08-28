c2132ea52056b5580c43ac8c6c70213b0d88cbfa
Nathaniel Blair-Stahn <ndbs@uw.edu>
2026-08-28 16:28:57 -0700

    Update anemia/iron notebook to work with Vivarium updates and GBD 2023 (#12)
    * update pregnant population notebook
    
    * remove Hgb distribution CDF correction since it was fixed upstream
    
    * factor out some notebook code into new gbd_data module
    
    * add warning about keeping __init__.py empty for compatibility with both environments
    
    * save executed rice/India/anemia notebook after updates
    
    * update anemia notebook to monorepo and GBD 2023
    
    * edit comment about draws; use explicit dataframe name in asserts
    
    * run make format

--- src/lsff_utils/hemoglobin_distribution.py
+++ src/lsff_utils/hemoglobin_distribution.py
@@ -32,9 +32,7 @@ def hemoglobin_cdf_from_mean_sd(mean, sd):
 
     def cdf(x):
         gamma_cdf = hemoglobin_distribution_gamma_part.cdf(x)
-        # NOTE: There is a bug in this CDF function -- it is reversed!
-        # https://github.com/ihmeuw/risk_distributions/issues/62
-        mgumbel_cdf = 1 - hemoglobin_distribution_mgumbel_part.cdf(x)
+        mgumbel_cdf = hemoglobin_distribution_mgumbel_part.cdf(x)
         return (
             GAMMA_DISTRIBUTION_WEIGHT * gamma_cdf
             + MIRROR_GUMBEL_DISTRIBUTION_WEIGHT * mgumbel_cdf
